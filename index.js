192e8a0f57bc7cd54615a21e4d02e990e4fee51c
jflopezr11 <joshualopezroque@gmail.com>
2021-04-21 14:57:06 -0700

    new push first day

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-//Think of this as my js snippet file? Will be using this to practice JS. Don't like JS - but it's not going anywhere. 
+//Think of this as my js snippet file? Will be using this to practice JS. Don't like JS - but it's not going anywhere- so I gotta learn it... again.
 
+//If Statments
+// Example 1
+if (1 === 1) {
+	console.log('Correct!');
+}
+
+//Example 2
+let rating = 3;
+
+if (rating === 3) {
+	console.log('Meh!');
+}
+
+// Example 3
+
+let num = 37;
+
+if (num % 2 !== 0) {
+	console.log('odd number!');
+}
