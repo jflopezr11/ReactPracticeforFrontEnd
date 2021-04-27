98bdac9c3028e1d8ec354d00f38eb119a98e3273
jflopezr11 <joshualopezroque@gmail.com>
2021-04-27 12:34:38 -0700

    practicing JS

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ if (1 === 1) {
 }
 
 //Example 2
-let rating = 3;
+let rating1 = 3;
 
-if (rating === 3) {
+if (rating1 === 3) {
 	console.log('Meh!');
 }
 
@@ -19,4 +19,63 @@ let num = 37;
 
 if (num % 2 !== 0) {
 	console.log('odd number!');
+}
+
+// ElseIF
+
+/* Performance Review! 
+3- Rock Star! 
+2- meh! 
+1- 'you gotta pick it up, kid' 
+*/
+
+let rating = Math.floor(Math.random() * 3) + 1;
+
+if (rating === 3) {
+	console.log('Rock Star!');
+} else if (rating === 2) {
+	console.log('meh!');
+} else if (rating === 1) {
+	console.log('you gotta pick it up, kid');
+} else {
+	console.log('do you even work here?');
+}
+
+//example 2
+// High Score?
+
+let highScore = 1500;
+let currentUserScore = 1501;
+
+if (currentUserScore >= highScore) {
+	console.log(`Congratulations you have the High Score: ${currentUserScore}!`);
+	highScore = currentUserScore;
+} else {
+	console.log(`Your current score is: ${currentUserScore}`);
+}
+
+// Nesting conditionals.
+
+//example 1
+
+let password = 'Joshua Lopez';
+
+if (password.length >= 6) {
+	if (password.indexOf(' ') === -1) {
+		console.log('Valid Password');
+	} else {
+		console.log('Password cannot contain spaces!');
+	}
+} else {
+	console.log('Password is too short!');
+}
+
+let jasonOwes = 120;
+jasonOwes = jasonOwes - 46;
+
+console.log(jasonOwes);
+
+let number = Number(prompt('Pick a number!'));
+if (!Number.isNaN(number)) {
+	console.log('Your number is the square root of ' + number * number);
 }
