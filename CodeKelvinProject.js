7847611381ab7e794ff37ced2c332c9e8460b72f
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2021-05-15 18:34:14 -0700

    Codecademy Projects

---
+++ CodeKelvinProject.js
@@ -0,0 +1,8 @@
+// kelvin will be remain constant
+const kelvin = 293;
+//subtracting kelvin - celsius
+let celsius = kelvin - 273;
+// solving for fahrenheit
+let fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+//console.log 
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
