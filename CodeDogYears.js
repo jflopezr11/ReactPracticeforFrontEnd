2e0b353f7ee35981b6113a6aab4f115114109fc5
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2021-05-15 18:35:55 -0700

    Codecademy project 2

---
+++ CodeDogYears.js
@@ -0,0 +1,18 @@
+// my acutal age
+let myAge = 30;
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+// comment
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+const myName = ('Joshua');
+console.log(myName.toLowerCase());
+
+console.log(`My name is ${myName} I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
